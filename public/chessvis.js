5e5d171994d3951c8d40464bc1fca03eff3f2245
Andrej Dravecky <andrejd@pop-os.localdomain>
2022-01-09 03:24:46 +0100

    data binding

--- public/chessvis.js
+++ public/chessvis.js
@@ -1,40 +1,126 @@
 "use strict";
 
 class MoveNode {
-    constructor() {
+    constructor(parent) {
         this.wwins = 0;
         this.bwins = 0;
         this.draws = 0;
+        this.total = 0;
+        this.parent = parent;
         this.moves = {};
     }
 
     insert_moves(moves, result) {
         this[['draws','wwins','bwins'].at(eval(result))] += 1;
+        this.total += 1;
         if(moves.length > 0 ) {
             if(moves[0] in this.moves) {
                 this.moves[moves[0]]
                     .insert_moves(moves.slice(1,-1), result);
             } else {
-                this.moves[moves[0]] = new MoveNode();
+                this.moves[moves[0]] = new MoveNode(this);
                 this.moves[moves[0]]
                     .insert_moves(moves.slice(1,-1),result);
             }
         }
     }
+
+    get_node(move) {
+        return this.moves.hasOwnProperty(move) ? 
+            this.moves[move] : new MoveNode(this);
+    }
+}
+
+class MoveBar {
+    constructor() {
+        this.moves = [];
+    }
+    
+    make_move(move) {
+        this.moves.push(move)
+    }
+
+    prev() {
+        this.moves.pop()
+    }
+    toString() {
+        return this.moves.join(" > ")
+    }
 }
 
 var gameData = [];
-var gameTree = new MoveNode();
+var gameTree = new MoveNode(null);
+var currentNode = gameTree;
+var moveBar = new MoveBar();
 
+var moveStats = null;
+
+window.onload = init;
+
+  //  .sort((a,b) => d3.descending( currentNode.moves[a].total, 
+//    currentNode.moves[b].total )
+//)
+
+
+function move_evnt(evnt) {
+    update(evnt)
+}
+
+function update(move) {
+    if(move === "") {
+        currentNode = gameTree;
+    } else {
+        currentNode = currentNode.get_node(move);
+    }
+    moveStats?.remove();
+    moveStats = d3.select("#move_stats")
+        .selectAll("div")
+        .data(Object.keys(currentNode.moves))
+        .enter()
+        .append('div')
+        .attr("id","bar");
+
+    moveStats.append("div")
+        .attr("id", "move")
+        .style("width", "30px")
+        .text(d => d)
+        .on("click", update);
+
+    moveStats.insert("div")
+        .style("background-color", "white");
+    
+    moveStats.insert("div")
+        .attr("id", "score")
+        .style("width", "30px")
+        .text(d => currentNode.moves[d].total);
+
+    moveStats.sort((a,b) => d3.descending(
+            currentNode.moves[a].total,
+            currentNode.moves[b].total ));
+    
+    moveStats.transition()
+
+
+    console.log(`update: ${move}`)
+      
+}
 
 function parsePgn(pgnStr) {
+    gameData = [];
+    gameTree = new MoveNode(null);
+
+    console.log("parsing started...");
     // parse PGN to datastructure
     // match game ((?:\[)(?:.|\s)+?\s[01]-[10])
     // match moves \d+\. ([A-Za-z].+?)\s(?:([A-Za-z].+?)\s)?
     // match attributes (?:\[(\w+)\s"(.+)"])
     
-    var games = pgnStr.split(/\s(0\-1|1\-0|1\/2-1\/2)/); //match(/((?:\[)(?:.|\s)+?\s[01]-[10])/g)
+    var games = pgnStr
+        .split(/\s(0\-1|1\-0|1\/2-1\/2)/)
+        .filter((item) => { return item.length > 9; });
+    var counter = 1;
     for(let gameStr of games) {
+        console.log(`parsing game ${counter++}/${games.length}`);
         var moves = gameStr
             .split(/\d+\. /)
             .slice(1,-1)
@@ -58,6 +144,7 @@ function parsePgn(pgnStr) {
         gameData.push(gameObj);
         gameTree.insert_moves(gameObj.moves, gameObj.result)
     }
+    update("")
 }
 
 // Shows 
@@ -100,15 +187,9 @@ function createChessBoard() {
 }
 
 function init(evnt) {
-    document.getElementById('chessboard')
-        .appendChild(createChessBoard());
+    document
+        .getElementById('chessboard')
+        .appendChild( createChessBoard() );
     $("td").addClass("tile");
     document.getElementById("upBtn").onclick = uploadPgn;
-
-    
 }
-
-window.onload = init
-
-
-
